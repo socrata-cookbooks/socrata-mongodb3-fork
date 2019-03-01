a12eff723800f82e81c94e894038cc8c6a446027
Marc Slemko <marc.slemko@socrata.com>
2019-03-01 13:33:16 -0800

    version bump for build issues.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sysadmin@socrata.com'
 license          'Apache 2.0'
 description      'Installs/Configures mongodb3'
 long_description 'Installs/Configures mongodb3'
-version          '999.0.0'
+version          '999.0.1'
 
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '= 7.8'
